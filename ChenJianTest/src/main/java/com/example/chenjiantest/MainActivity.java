cc7802c69bb8045ee424a6ebe3650cef38aa9210
hellojian123 <570963791@qq.com>
2013-07-30 09:56:31 +0800

    修正Windows平台上的编译乱码

--- ChenJianTest/src/main/java/com/example/chenjiantest/MainActivity.java
+++ ChenJianTest/src/main/java/com/example/chenjiantest/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.chenjiantest;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -19,5 +20,7 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+    public void getPeers(View view) {
+
+    }
 }
